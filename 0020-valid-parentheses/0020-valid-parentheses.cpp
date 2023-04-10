4f15c0f8a607bb1054f241a6465b550b383c655b
Mukund Tiwari <111234972+M-u-k-u-n-d@users.noreply.github.com>
2023-04-10 15:14:36 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (12.09%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n = s.length(),a=0,b=0,c=0;
         stack <char> st;
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<s.length(); i++) {
             if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
             else{
+//              IF STACK IS EMPTY AND s[i] IS A CLOSING BRACKET   
             if(st.size() == 0) return false;
-                 char ch = st.top();
+                 char ch = st.top(); // top element of stack 
             if(s[i] == ')'){
                 if(ch != '(') return false;
             }

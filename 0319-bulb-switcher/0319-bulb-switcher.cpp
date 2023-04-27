e0f6573914c68dbb2a7d14957d35c5d0fe3e64e7
Mukund Tiwari <111234972+M-u-k-u-n-d@users.noreply.github.com>
2023-04-27 18:19:03 +0530

    Time: 3 ms (45.34%), Space: 5.9 MB (63.99%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    // time complexity = O(nlog(n))
+    // time complexity = O(log(n))
     int bulbSwitch(int n) {
+//      All perfect square numbers have odd factor , find all perfect square number in n
       int ans = sqrt(n);
         return ans;
     }

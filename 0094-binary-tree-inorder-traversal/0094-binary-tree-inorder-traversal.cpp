51ca12ed6b6ac0e616ebbad7ec3bf62abe8c11f7
Mukund Tiwari <111234972+M-u-k-u-n-d@users.noreply.github.com>
2023-05-01 15:51:40 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (44.22%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,17 +10,27 @@
  * };
  */
 class Solution {
-//     RECURSIVE SOLUTION
-    vector<int> inorder(TreeNode* root,vector<int> &ans){
-        if(root == NULL) return ans;
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-        return ans;
-    }
+//     Iterative SOLUTION
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        return inorder(root,ans);
+        
+        TreeNode* node = root;
+        stack<TreeNode*> st;
+        
+        vector<int> ans;
+        while(true){
+            if(node != NULL){
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                if(st.empty()) break;
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
+        return ans;
     }
 };

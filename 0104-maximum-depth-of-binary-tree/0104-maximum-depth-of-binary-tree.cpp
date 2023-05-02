203af5d02af4d0da7f08a6c6ff7abf167d079485
Mukund Tiwari <111234972+M-u-k-u-n-d@users.noreply.github.com>
2023-05-02 16:03:00 +0530

    Time: 8 ms (73.31%), Space: 18.8 MB (92.32%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -10,29 +10,13 @@
  * };
  */
 class Solution {
-    
+    int f(TreeNode* root){
+        if(root == NULL) return 0;
+        return 1 + max(f(root->left) , f(root->right));
+    }
 public:
     int maxDepth(TreeNode* root) {
-          int ans = 1;
-          queue<TreeNode*> q;
-         if(root == NULL) return 0;
-            q.push(root);
-        while(!q.empty()){
-            queue<TreeNode*>v;
-            while(!q.empty()){
-            root = q.front();
-            q.pop();
-            if(root->left != NULL ) v.push(root->left);
-            if(root->right != NULL) v.push(root->right);
-            }
-            if(v.size() > 0){
-                ans++;
-                while(!v.empty()){
-                    q.push(v.front());
-                    v.pop();
-                }
-            }
-        }
-        return ans;
+          if(root == NULL) return 0;
+        return f(root);
     }
 };

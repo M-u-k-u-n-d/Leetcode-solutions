03df78652420d7fd1dce5b78b99e299ed08ca996
Mukund Tiwari <111234972+M-u-k-u-n-d@users.noreply.github.com>
2023-05-30 17:30:55 +0530

    Time: 14 ms (9.31%), Space: 18.1 MB (5.98%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -9,27 +9,18 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-    TreeNode* f(vector<int>preorder,int prestart,int preend,vector<int>inorder,int instart,int inend,map<int,int>mpp){
-        if(prestart > preend || instart > inend) return NULL;
-        int inroot = mpp[preorder[prestart]];
-        int numsleft = inroot - instart;
-        TreeNode* root = new TreeNode(inorder[inroot]);
-        root->left = f(preorder,prestart+1,prestart+numsleft,inorder,instart,inroot-1,mpp);
-        root->right = f(preorder,prestart+numsleft+1,preend,inorder,inroot+1,inend,mpp);
+class Solution {   
+    TreeNode* f(vector<int>preorder, int &i,int ub){
+       if(i==preorder.size() || preorder[i] > ub) return NULL;
+        TreeNode* root = new TreeNode(preorder[i++]);
+        root->left = f(preorder,i,root->val);
+        root->right =  f(preorder,i,ub);
         return root;
     }
-   
+        
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        vector<int> inorder = preorder;
-        sort(inorder.begin(),inorder.end());
-        int instart = 0, inend = inorder.size()-1, prestart = 0, preend = preorder.size()-1;
-        // TreeNode* root = new TreeNode(preorder[0]);    
-        map<int,int>mpp;
-        for(int i=0; i<inorder.size(); i++){
-            mpp[inorder[i]] = i;
-        }
-       return f(preorder,prestart,preend,inorder,instart,inend,mpp);
+        int i=0;
+       return f(preorder,i,INT_MAX);
     }
 };

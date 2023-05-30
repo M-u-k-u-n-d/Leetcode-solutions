4abb5de7c21c1e61b7b13f5ffb8957d1967bbe33
Mukund Tiwari <111234972+M-u-k-u-n-d@users.noreply.github.com>
2023-05-30 21:05:49 +0530

    Time: 28 ms (78.88%), Space: 24 MB (66.98%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -9,30 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-TreeNode* f(queue<TreeNode*>&Q,TreeNode* root){
-    if(root == NULL) return NULL;
-    if(root->left) f(Q,root->left);
-    Q.push(root);
-    if(root->right) f(Q,root->right);
-    return root;
-}
+
 class BSTIterator {
-    queue<TreeNode*>Q;
+    stack<TreeNode*>st;
 public:
     BSTIterator(TreeNode* root) {
-      root = f(Q,root);
+     while(root != NULL) {
+        st.push(root);
+         root = root->left;
+     }
     }
     
     int next() {
-        // cout<<Q.front()->val<<" ";
-        TreeNode* cur = Q.front();
-        Q.pop();
-        return cur->val;
-        // return 0;
-    }
-    
+      TreeNode* curr = st.top();
+        int num = curr->val;
+        st.pop();
+            if(curr->right){
+                curr=curr->right;
+                    while(curr != NULL) {
+                        st.push(curr);
+                        curr = curr->left;
+                                   }
+                    }
+        return num;
+        }
     bool hasNext() {
-        return !Q.empty();
+        return !st.empty();
     }
 };
 

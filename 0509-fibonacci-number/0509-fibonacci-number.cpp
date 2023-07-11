9f0c31cb540729361f847fb7dfb2e5c5a94fda84
Mukund Tiwari <111234972+M-u-k-u-n-d@users.noreply.github.com>
2023-07-11 20:50:36 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (93.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,16 @@
 class Solution {
+    int f(int n, int dp[]){
+        if(n <= 1) return n;
+        if(dp[n] == -1){
+        dp[n] = f(n-1,dp)+f(n-2,dp);
+            return dp[n];
+        }
+        return dp[n];
+    }
 public:
     int fib(int n) {
-        if(n <= 1) return n;
-        return fib(n-1)+fib(n-2);
+        int dp[n+1];
+        memset(dp,-1,sizeof dp);
+        return f(n,dp);
     }
 };

d8ff48036e1ec4d6e181782c3ae4b70d70c148e7
Mukund Tiwari <111234972+M-u-k-u-n-d@users.noreply.github.com>
2023-07-26 23:35:05 +0530

    Time: 4 ms (56.51%), Space: 7.8 MB (61.06%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,16 +1,24 @@
 class Solution {
-    int f(vector<vector<int>> &dp, int row , int col , int m, int n ,vector<vector<int>> arr){
-    if(row >= m || col >= n) return 0;
-    if(arr[row][col] == 1) return 0;    
-    if(row == m-1 and col == n-1) return 1;
-    if(dp[row][col] != -1) return dp[row][col];
-    dp[row][col] = f(dp,row+1,col,m,n,arr) + f(dp,row,col+1,m,n,arr);
-    return dp[row][col];
-    }
+
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size(),n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        return f(dp,0,0,m,n,obstacleGrid);
+        vector<vector<int>> dp(m, vector<int> (n,0));
+        if(obstacleGrid[m-1][n-1] == 1) return 0;
+        dp[m-1][n-1] = 1;
+        for(int i=m-1; i>=0; i--){
+            for(int j = n-1; j>=0; j--){
+                if(i==m-1 and j==n-1) continue;
+                long long val1 = 0, val2 = 0;
+                 if(i != m-1) val1 = dp[i+1][j];
+                if(j != n-1) val2 = dp[i][j+1];
+                if(obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                }
+                else
+                    dp[i][j] = val1 + val2;
+            }
+        }
+        return dp[0][0];
     }
 };

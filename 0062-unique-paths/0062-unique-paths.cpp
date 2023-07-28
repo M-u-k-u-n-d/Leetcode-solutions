359f3bd696a6896157f02d4d9ce7341ff62e454f
Mukund Tiwari <111234972+M-u-k-u-n-d@users.noreply.github.com>
2023-07-28 10:06:04 +0530

    Time: 3 ms (38.26%), Space: 6.1 MB (75.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,17 +2,22 @@ class Solution {
 // Tabulation
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n,0));
-        dp[m-1][n-1] = 1;
+        // vector<vector<int>> dp(m, vector<int> (n,0));
+        vector<int> dp(n,0);
         for(int i=m-1; i>=0; i--){
+            vector<int> tmp(n);
             for(int j = n-1; j>=0; j--){
                 int val1 =0, val2 =0;
-                if(i==m-1 and j == n-1) continue;
-                if(i != m-1) val1 = dp[i+1][j];
-                if(j != n-1) val2 = dp[i][j+1];
-                 dp[i][j] = val1+val2;
+                if(i==m-1 and j == n-1) {
+                    tmp[j] = 1;
+                    continue;
+                }
+                if(i != m-1) val1 = dp[j];
+                if(j != n-1) val2 = tmp[j+1];
+                 tmp[j] = val1+val2;
             }
+            dp = tmp;
         }
-        return dp[0][0];
+        return *max_element(dp.begin(),dp.end());
     }
 };

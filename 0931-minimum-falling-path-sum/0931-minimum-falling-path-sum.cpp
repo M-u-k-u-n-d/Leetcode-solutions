f8fe218656cf56d8c899537aa035ef85f7561efb
Mukund Tiwari <111234972+M-u-k-u-n-d@users.noreply.github.com>
2023-07-28 22:40:04 +0530

    Time: 12 ms (93.96%), Space: 10.7 MB (38.70%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,22 +1,23 @@
 class Solution {
-    int f(int row,int col, vector<vector<int>>& arr, int n,vector<vector<int>>& dp){
-        if(row == n-1) return arr[row][col];
-        if(dp[row][col] != INT_MAX) return dp[row][col];
-        int num1 = INT_MAX,num2 = INT_MAX,num3 = INT_MAX;
-        if(col < n-1) num1 = f(row+1,col+1,arr,n,dp);
-        if(col > 0) num2 = f(row+1,col-1,arr,n,dp);
-        num3 = f(row+1,col,arr,n,dp);
-        
-        return dp[row][col] = arr[row][col] + min(num1,min(num2,num3));
-        
-    }
+ 
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int ans = INT_MAX;
         int n = matrix.size();
         vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
-        for(int i=0; i<n; i++)
-        ans = min(ans, f(0,i,matrix,matrix.size(),dp));
-        return ans;
+        for(int i=0; i<n; i++){
+            dp[n-1][i] = matrix[n-1][i];
+        }
+        for(int row =n-2; row >= 0; row--){
+            for(int col = 0; col < n; col++){
+             int num1 = INT_MAX,num2 = INT_MAX,num3 = INT_MAX;
+                if(col < n-1) num1 = dp[row+1][col+1];
+                if(col > 0) num2 = dp[row+1][col-1];
+                num3 = dp[row+1][col];
+                dp[row][col] = matrix[row][col] + min(num1,min(num2,num3));
+            }
+        }
+ 
+        return *min_element(dp[0].begin(),dp[0].end());
     }
 };

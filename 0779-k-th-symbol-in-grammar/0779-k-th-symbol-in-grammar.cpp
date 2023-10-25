70b9dd3249645c091bbe38798c990ee5e331941e
Mukund Tiwari <111234972+M-u-k-u-n-d@users.noreply.github.com>
2023-10-25 17:57:21 +0530

    Time: 2 ms (51.37%), Space: 6.3 MB (53.37%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    
+    int solve(int n, int k){
+        if(n == 1 and k == 1) return 0;
+        
+        int mid = pow(2,n-1) / 2;
+        if(k <= mid) return solve(n-1,k);
+        else return !solve(n-1,k-mid);
+    }
+public:
+    int kthGrammar(int n, int k) {
+        return solve(n,k);
+    }
+};
